b013336c28608ab01f9a98bec9a9e52e4f8108fa
Jyothish <maddinenijyothish37@gmail.com>
2026-09-24 22:18:54 +0530

    Simple rocket countdown

---
+++ myprojects/src/com/practice/SimpleRocketCountdown.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+
+public class SimpleRocketCountdown {
+
+	public static void main(String[] args) {
+		int count = 10;
+		while(count>=1) {
+			System.out.println(count);
+			count--;
+		}
+		System.out.println("Blast off!!");
+		
+
+	}
+
+}
