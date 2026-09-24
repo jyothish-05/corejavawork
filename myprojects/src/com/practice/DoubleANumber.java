c34afd42679d15d9956e96b7a888c46ca595eefd
Jyothish <maddinenijyothish37@gmail.com>
2026-09-24 22:12:20 +0530

    double a number using while loop

---
+++ myprojects/src/com/practice/DoubleANumber.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class DoubleANumber {
+
+	public static void main(String[] args) {
+		int n = 1;
+		while(n<=100) {
+			System.out.println(n);
+			n*=2;
+		}
+
+	}
+
+}
